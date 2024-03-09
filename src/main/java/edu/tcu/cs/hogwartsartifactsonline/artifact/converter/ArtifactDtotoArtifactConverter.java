610cb00107df108ec8c6231a29ea11edcdcdfac4
Genesect01 <wapeck88@gmail.com>
2024-03-08 22:38:55 -0600

    Artifact CRUD done! Close #1

---
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/converter/ArtifactDtotoArtifactConverter.java
@@ -0,0 +1,20 @@
+package edu.tcu.cs.hogwartsartifactsonline.artifact.converter;
+
+import edu.tcu.cs.hogwartsartifactsonline.artifact.Artifact;
+import edu.tcu.cs.hogwartsartifactsonline.artifact.dto.ArtifactDto;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class
+ArtifactDtotoArtifactConverter implements Converter<ArtifactDto, Artifact> {
+    @Override
+    public Artifact convert(ArtifactDto source) {
+        Artifact artifact = new Artifact();
+        artifact.setId(source.id());
+        artifact.setName(source.id());
+        artifact.setDescription(source.id());
+        artifact.setImageUrl(source.id());
+        return artifact;
+    }
+}
